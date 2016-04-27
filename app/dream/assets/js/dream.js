84aea986d17861ef30e7ae3df597438f6d47d9af
Gifa Eriyanto <gifaeriyanto@ymail.com>
2016-04-27 11:14:39 +0800

    base

--- app/dream/assets/js/dream.js
+++ app/dream/assets/js/dream.js
@@ -11248,3 +11248,30 @@ window.Modernizr = (function( window, document, undefined ) {
 
 })(this, this.document);
 
+
+// Background Generator
+
+$(".bg").each(function() {
+	var bgColor = $(this).attr("bg-color");
+	$('.bg[bg-color="' + bgColor + '"]').css("background-color",bgColor);
+
+	var bgImage = $(this).attr("bg-image");
+	$('.bg[bg-image="' + bgImage + '"]').css("background-image","url(" + bgImage + ")");
+});
+$(".dream *").each(function() {
+
+	// Background Generator
+	var bgColor = $(this).attr("bg-color");
+	$('[bg-color="' + bgColor + '"]').css("background-color", bgColor);
+
+	var bgImage = $(this).attr("bg-image");
+	$('[bg-image="' + bgImage + '"]').css("background-image", "url(" + bgImage + ")");
+
+	// Color Generator
+	var color = $(this).attr("color");
+	$('[color="' + color + '"]').css("color", color);
+
+	// Font Generator
+	var size = $(this).attr("size");
+	$('[size="' + size + '"]').css("font-size", size);	
+});
