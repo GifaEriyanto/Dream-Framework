90d67e09b5bd1680e23a2d19c8d6c861a2e40fbe
Gifa Eriyanto <gifaeriyanto@ymail.com>
2016-05-03 18:32:19 +0800

    aligment classes

--- app/dream/assets/js/dream.js
+++ app/dream/assets/js/dream.js
@@ -11251,17 +11251,18 @@ window.Modernizr = (function( window, document, undefined ) {
 $(".dream *").each(function() {
 
 	// Background Generator
-	var bgColor = $(this).attr("bg-color");
-	$('[bg-color="' + bgColor + '"]').css("background-color", bgColor);
+		var bgColor = $(this).attr("bg-color");
+		$('[bg-color="' + bgColor + '"]').css("background-color", bgColor);
 
-	var bgImage = $(this).attr("bg-image");
-	$('[bg-image="' + bgImage + '"]').css("background-image", "url(" + bgImage + ")");
+		var bgImage = $(this).attr("bg-image");
+		$('[bg-image="' + bgImage + '"]').css("background-image", "url(" + bgImage + ")");
 
 	// Color Generator
-	var color = $(this).attr("color");
-	$('[color="' + color + '"]').css("color", color);
+		var color = $(this).attr("color");
+		$('[color="' + color + '"]').css("color", color);
 
 	// Font Generator
-	var size = $(this).attr("size");
-	$('[size="' + size + '"]').css("font-size", size);	
+		var size = $(this).attr("size");
+		$('[size="' + size + '"]').css("font-size", size);
+	
 });
--- assets/js/dream.js
+++ assets/js/dream.js
@@ -1,17 +1,18 @@
 $(".dream *").each(function() {
 
 	// Background Generator
-	var bgColor = $(this).attr("bg-color");
-	$('[bg-color="' + bgColor + '"]').css("background-color", bgColor);
+		var bgColor = $(this).attr("bg-color");
+		$('[bg-color="' + bgColor + '"]').css("background-color", bgColor);
 
-	var bgImage = $(this).attr("bg-image");
-	$('[bg-image="' + bgImage + '"]').css("background-image", "url(" + bgImage + ")");
+		var bgImage = $(this).attr("bg-image");
+		$('[bg-image="' + bgImage + '"]').css("background-image", "url(" + bgImage + ")");
 
 	// Color Generator
-	var color = $(this).attr("color");
-	$('[color="' + color + '"]').css("color", color);
+		var color = $(this).attr("color");
+		$('[color="' + color + '"]').css("color", color);
 
 	// Font Generator
-	var size = $(this).attr("size");
-	$('[size="' + size + '"]').css("font-size", size);	
+		var size = $(this).attr("size");
+		$('[size="' + size + '"]').css("font-size", size);
+	
 });
